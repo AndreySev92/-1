7ee6f8bd899f66061e46dcba51d9c966e976a062
AndreySev92 <kovaleff.14@yandex.ru>
2025-03-28 14:30:54 +0300

    Задание 1.1.4(Готовое)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,12 +2,23 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.model.User;
-
-import java.util.List;
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Nikita", "Doe", (byte) 25);
+        userService.saveUser("Joe", "7A35090F", (byte) 43);
+        userService.saveUser("Max", "Alderson", (byte) 42);
+        userService.saveUser("Maxim", "Engressia", (byte) 44);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.closeSessionFactory();
+
+
 
     }
 }
